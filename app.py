725c99a5e4179aa8253d63c1adb0c60ba54313c3
Ferg <fergus.ampuan@gmail.com>
2025-06-11 23:12:00 +1200

    Uncomment input and output statements for user interaction

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import math
 # # print("Hello World 😺")
 # # print("*" * 10)
 
@@ -25,11 +26,36 @@
 # full = f"{first} {last}"
 # print(full)
 
-course = "Python Programing"
-print(course.upper())
-print(course.lower())
-print(course.title())
-print(course.strip())
-print(course.find("Pro"))
-print("pro" in course)
-print("swift" not in course)
+# course = "Python Programing"
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+# print(course.find("Pro"))
+# print("pro" in course)
+# print("swift" not in course)
+
+# Numbers
+# x = 1
+# x = 1.1
+# x = 1 + 2j
+
+# print(10 + 3)
+# print(10-3)
+# print(10 * 3)
+# print(10/3)
+# print(10//3)
+# print(10 % 3)
+# print(10 ** 3)
+
+# x = 10
+# x = x+3
+# x += 3
+
+# print(round(2.9))
+# print(abs(-2.9))
+# print(math.ceil(2.2))
+
+x = input("x: ")
+y = int(x)+1
+print(f"x:{x}, y: {y}")
