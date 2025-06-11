b53af366aca2a35942129285c87a879fc5089535
Ferg <fergus.ampuan@gmail.com>
2025-06-11 22:25:30 +1200

    Refactor print statements and organize variable definitions

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-print("Hello World 😺")
-print("*" * 10)
+# print("Hello World 😺")
+# print("*" * 10)
 
 x = 1
 y = 2
 unit_price = 3
+
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Python Programming"
+# print(students_count)
+print(len(course_name))
+print(course_name[0])
+print(course_name[-1])
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
